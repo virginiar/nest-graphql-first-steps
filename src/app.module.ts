95bf1bf8d33fe396bd3dbda0b8d97e6e1fa72bfa
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-21 12:43:21 +0200

    Resolver HelloWorld

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // debug: false,
-      // graphiql: false
+      // graphiql: true,
+      // playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    HelloWorldModule,
   ],
   controllers: [],
   providers: [],
