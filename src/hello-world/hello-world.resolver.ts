9d167d6fd3c2a1e539345775fef7fc8b33c188ee
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-21 15:03:10 +0200

    Añadiendo argumentos a un resolver

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,4 +1,4 @@
-import { Float, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
@@ -15,8 +15,13 @@ export class HelloWorldResolver {
     return Math.random() * 100;
   }
 
-  @Query(() => Int, { name: 'randomFromZeroTo' })
-  getRandomFromZeroTo(): number {
-    return Math.floor(Math.random() * 10);
+  @Query(() => Int, {
+    name: 'randomFromZeroTo',
+    description: 'From zero to argument TO (default 6)',
+  })
+  getRandomFromZeroTo(
+    @Args('to', { nullable: true, type: () => Int }) to: number = 6,
+  ): number {
+    return Math.floor(Math.random() * to);
   }
 }
